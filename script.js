e73bbed842f0aae0f93f3531fae2f09623651c6b
Samantha Shapland <samshapland@gmail.com>
2021-10-13 11:57:23 -0400

    added js file

---
+++ script.js
@@ -0,0 +1,16 @@
+//STEP 1 <-------------------------------------------FETCH DATA------------------------------------------------------->
+/*
+
+
+*/
+//STEP 2 <------------------------USE DATA TO ADD CHARACTER NAMES TO THE DROPDOWN MENU------------------------------->
+/*
+
+
+*/
+//STEP 3 <--------------------------------DISPLAY SELECTED CHARACTER'S INFO------------------------------------------->
+/*
+
+
+*/
+//STEP 4 <------------------ADD SELECTED CHARACTER'S NAME TO TEAM MEMBERS LIST ON BUTTON CLICK------------------------>
