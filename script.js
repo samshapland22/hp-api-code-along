20840fcfa8bf3138702d0e9c79331ccf2bda2811
Samantha Shapland <samshapland@gmail.com>
2021-10-13 16:44:27 -0400

    fetched data

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 //STEP 1 <-------------------------------------------FETCH DATA------------------------------------------------------->
-/*
+fetch("http://hp-api.herokuapp.com/api/characters/house/gryffindor")
+  .then((res) => {
+    //parse the json to make it a javascript object so that we can use it.
+    return res.json();
+  })
+  .then((characters) => {
+    console.log(characters);
+    //STEP 2 <------------------------USE DATA TO ADD CHARACTER NAMES TO THE DROPDOWN MENU------------------------------->
+  });
 
-
-*/
-//STEP 2 <------------------------USE DATA TO ADD CHARACTER NAMES TO THE DROPDOWN MENU------------------------------->
-/*
-
-
-*/
 //STEP 3 <--------------------------------DISPLAY SELECTED CHARACTER'S INFO------------------------------------------->
-/*
 
-
-*/
 //STEP 4 <------------------ADD SELECTED CHARACTER'S NAME TO TEAM MEMBERS LIST ON BUTTON CLICK------------------------>
