87fadf2bb68528e34e5c626a7978a3ddd7b50463
Samantha Shapland <samshapland@gmail.com>
2021-10-13 17:34:54 -0400

    added names to dropdown menu

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ fetch("http://hp-api.herokuapp.com/api/characters/house/gryffindor")
   .then((characters) => {
     console.log(characters);
     //STEP 2 <------------------------USE DATA TO ADD CHARACTER NAMES TO THE DROPDOWN MENU------------------------------->
+
+    let dropDown = document.querySelector("#dropdown");
+
+    //add each character's name from the array of data called "characters" to the dropdown menu
+    for (let character of characters) {
+      const option = document.createElement("option");
+      option.setAttribute("value", character.name);
+      option.textContent = character.name;
+      dropDown.append(option);
+    }
   });
 
 //STEP 3 <--------------------------------DISPLAY SELECTED CHARACTER'S INFO------------------------------------------->
